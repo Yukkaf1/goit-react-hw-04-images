ee54b767d2d82284ceb8b2e69e3e25544cb34a58
Trunova Yulia <103053349+Yukkaf1@users.noreply.github.com>
2023-05-20 11:29:01 +0200

    axios

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 
-const API_KEY = '22104578-b37830bb47769ec8fcc7503cc';
+const API_KEY = '33761438-9314d2b90b41fb92b07a88ae9';
 export const perPage = 12;
 
 export const getImages = async (query, page) => {
